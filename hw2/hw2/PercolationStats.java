ea05a9a4be93407897f90b1bc11fa4cef68b86fb
SirW02 <3227424492@qq.com>
2024-06-09 19:34:43 +0800

    Completed hw02

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -12,14 +12,14 @@ public class PercolationStats {
     private double[] fraction;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
+
         this.N = N;
         this.T = T;
         this.pf = pf;
-        fraction = new double[T];
-
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
         }
+        fraction = new double[T];
 
         for (int i = 0; i < T; i++) {
             pc = pf.make(N);
@@ -48,4 +48,10 @@ public class PercolationStats {
     public double confidenceHigh() {
         return mean() + 1.96 * stddev() / Math.sqrt(T);
     }
+
+//     public static void main(String[] args) {
+//     PercolationStats ps = new PercolationStats(23, -1, new PercolationFactory());
+//     System.out.println(ps.mean());
+//     System.out.println(ps.stddev());
+//     }
 }
