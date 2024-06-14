fdf5ba0d3763151bc7fdcb221c4ff969bc78259b
SirW02 <3227424492@qq.com>
2024-06-15 02:02:18 +0800

    Completed hw3

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -26,7 +26,7 @@ public class SimpleOomage implements Oomage {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            return 31 * 31 * red + 31 * green + blue;
+            return 51 * 51 * red / 5 + 51 * green / 5 + blue / 5;
         }
     }
 
