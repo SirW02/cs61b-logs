4d1c347f822bb15bfd6bb4c625ac2fcfa8d11cb9
SirW02 <3227424492@qq.com>
2024-06-15 20:45:48 +0800

    Completed lab10

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -104,6 +104,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      * Bubbles up the node currently at the given index.
      */
     private void swim(int index) {
+        if (index == 1) {
+            return;
+        }
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
@@ -118,6 +121,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      * Bubbles down the node currently at the given index.
      */
     private void sink(int index) {
+        if (!inBounds(index)) {
+            return;
+        }
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
